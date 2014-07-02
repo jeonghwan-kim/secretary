a7624efe2cd13af126f6cbf2447ebef283af6f14
jeonghwan <ej88ej@gmail.com>
2014-07-02 17:18:59 +0900

    Featured: 디자인 작업 완료.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,4 +5,15 @@ angular.module('secretaryApp')
 
     $scope.btnName = '저장하기';
 
+    $scope.submit = function() {
+      var email = $scope.email,
+        todo = $scope.todo,
+        msg = "";
+
+      console.log(email, todo);
+
+      $scope.msg = '네 알겠습니다. <span class="label label-theme-red">' + todo + '</span>을 기억하고 있겠습니다.';
+
+    };
+
   });
